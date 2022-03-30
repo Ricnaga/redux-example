25bdd7a0facea58c6f2d75e54f2cc653c96daf78
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-03-30 20:30:57 -0300

    initial commit

---
+++ src/components/Catalog/Catalog.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export function Catalog() {
+  const state = useSelector((state) => state);
+  console.log(state);
+  return <h1>Catalog</h1>;
+}
