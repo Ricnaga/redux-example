a19082cd74dc0d152eb230715cd100710fb91154
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-03-31 08:41:51 -0300

    feat:create mocked server and routes in frontend

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { IProduct } from "../../store/modules/cart/types";
 
 export function Catalog() {
-  const state = useSelector((state) => state);
-  console.log(state);
-  return <h1>Catalog</h1>;
+  const [catalog, setCatalog] = useState<Array<IProduct>>([]);
+
+  useEffect(() => {
+    api.get("products").then((response) => setCatalog(response.data));
+  }, []);
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong>
+          <span>{product.price}</span>
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
 }
